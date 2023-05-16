1aa24837744035f0dabcc505042be2cf3162142b
dhaiphan <hai.phan@duodecim.fi>
2023-05-16 14:21:49 +0300

    Tower Swing 0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: netlify({
+    dist: new URL('./dist/', import.meta.url)
+  })});
